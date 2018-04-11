7509016a0e02df15af896f1bd9ad43c91c83f456
GusFr <35657277+GusFr@users.noreply.github.com>
2018-04-11 18:47:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ bot.on("message", function (message) {
             .setTitle("Liste des Commandes")
             .setDescription("Liste des commandes du BOT")
             .addField("- /help","**Page d'aide**", true)
-            .setColor("0x96CA2D")
-            .setFooter("Good")
+            .setColor("0x009127")
+            .setFooter("©Menta PvP - 2018 Tout droits réservés")
         message.channel.sendEmbed(embed);
     }  
 
